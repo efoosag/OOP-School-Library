5c8e283a56a55877e8491e8d9fe3fb6eee7d82e5
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-28 23:00:01 +0100

    fix indentation

--- Student.rb
+++ Student.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 require './Person'
 
 class Student < Person
-    def initialize(name, age, classroom, parent_permission:true)
-        super(name, age, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+  def initialize(name, age, classroom, parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        puts '¯\(ツ)/¯'
-    end
+  def play_hooky
+    puts '¯\(ツ)/¯'
+  end
 end
-
--- Teacher.rb
+++ Teacher.rb
@@ -1,15 +1,14 @@
+# frozen_string_literal: true
+
 require './Person'
 
 class Teacher < Person
-    def initialize(name, age, specialization)
-        super(name, age)
-        @specialization = specialization
-    end
+  def initialize(name, age, specialization)
+    super(name, age)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
-
-t = Teacher.new('tunde', 5, 'math')
-t.can_use_services()
