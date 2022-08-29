c272abb183edd387877505665263517bc7d8c07a
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-29 13:13:55 +0100

    fix indentation error

--- nameable.rb
+++ nameable.rb
@@ -1,29 +1,28 @@
 class Nameable
-    def correct_name
-        raise NotImplementedError
-    end
+  def correct_name
+    raise NotImplementedError
+  end
 end
 
 class BaseDecorator < Nameable
-    
-    def initialize(nameable)
-       super()
-       @nameable = nameable
-    end
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
 
 class CapitalizeDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
 
-class  TrimmerDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.length <= 10? @nameable.correct_name : @nameable.correct_name[0..9]
-    end
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.length <= 10 ? @nameable.correct_name : @nameable.correct_name[0..9]
+  end
 end
