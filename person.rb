08e58af8a78f9fee8b088e2d0ae834f243d9e09f
efoosag <96612103+efoosag@users.noreply.github.com>
2022-08-29 22:17:12 +0100

    update based on review

--- person.rb
+++ person.rb
@@ -1,16 +1,17 @@
 require './nameable'
 
 class Person < Nameable
+  attr_reader :id, :rental
+  attr_accessor :name, :age
+
   def initialize(age, name = `Unknown`, parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services?
     with_age || @parent_permission
@@ -18,6 +19,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
